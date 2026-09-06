56827503efad36c5759a5a3226dfbdee100673bd
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 00:15:47 +0400

    Emit Developer Platform at Cloudflare root

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  root: "public/developer",
+  publicDir: false,
   plugins: [react()],
   build: {
+    outDir: "../../dist",
+    emptyOutDir: true,
     chunkSizeWarningLimit: 900,
     rollupOptions: {
-      input: {
-        index: "public/developer/index.html",
-      },
+      input: "index.html",
       output: {
         manualChunks: {
           vendor: ["react", "react-dom"],
