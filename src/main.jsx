1e5a8840b1f79c763f5550fc064f25687e92c95a
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 19:10:52 +0400

    fix(developer): route /developer to fixed production workspace

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import PlusHub from "./plusHub.jsx";
-import DeveloperPlatform from "./DeveloperPlatform.jsx";
+import DeveloperPlatform from "./DeveloperPlatformFixed.jsx";
 import "./index.css";
 
 const isDeveloper = window.location.pathname === "/developer" || window.location.pathname.startsWith("/developer/");
