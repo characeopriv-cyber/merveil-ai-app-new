a9ab0fef017c3a35fa00e339f7ca4788908ea4f7
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 19:07:52 +0400

    feat: route developer workspace separately from citizen app

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import PlusHub from "./plusHub.jsx";
+import DeveloperPlatform from "./DeveloperPlatform.jsx";
 import "./index.css";
+
+const isDeveloper = window.location.pathname === "/developer" || window.location.pathname.startsWith("/developer/");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
-      <App />
-      <PlusHub />
-    </>
+    {isDeveloper ? <DeveloperPlatform /> : <><App /><PlusHub /></>}
   </React.StrictMode>
 );
