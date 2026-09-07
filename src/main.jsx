baf17f05263df080df286c8fca224a6392adf869
IVONIX CEO <characeopriv@gmail.com>
2026-09-07 19:12:18 +0400

    fix(developer): activate hardened developer workspace

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import PlusHub from "./plusHub.jsx";
-import DeveloperPlatform from "./DeveloperPlatformFixed.jsx";
+import DeveloperPlatform from "./DeveloperPlatformFixed2.jsx";
 import "./index.css";
-
-const isDeveloper = window.location.pathname === "/developer" || window.location.pathname.startsWith("/developer/");
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    {isDeveloper ? <DeveloperPlatform /> : <><App /><PlusHub /></>}
-  </React.StrictMode>
-);
+const isDeveloper=window.location.pathname==="/developer"||window.location.pathname.startsWith("/developer/");
+ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode>{isDeveloper?<DeveloperPlatform/>:<><App/><PlusHub/></>}</React.StrictMode>);
